5ded03929f515e045893840798fd0efb1e5d2e55
rkordiuk <rkordiuk@griddynamics.com>
2022-07-14 19:47:43 +0300

    updated tasks

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const mongoURI = process.env.MONGO_URI;
 
 const mongoose = require('mongoose');
+const e = require("express");
 const {Schema} = mongoose;
 
 
@@ -63,15 +64,137 @@ app.post('/api/users', (req, res) => {
           })
         });
       } else {
-        res.send("Username already Exists");
+        UserInfo.find({}).then(function (users) {
+          res.send(users);
+        });
+        // res.send("Username already Exists");
       }
     }
   });
 });
 
   // #2
-app.post('/api/users/:_id/exercise', (req, res) => {
+app.post('/api/users/:_id/exercises', (req, res) => {
   let idJson = {'id': req.params._id};
+  let checkedDate = new Date(req.body.date);
+  let idToCheck = idJson.id;
+
+  let noDateHandler = () => {
+    if (checkedDate instanceof Date && !isNaN(checkedDate)) {
+      return checkedDate;
+    } else checkedDate = new Date();
+  }
+
+
+  UserInfo.findById(idToCheck, (err, userData) => {
+    noDateHandler();
+
+    if (err) console.log("error with id =>", err);
+    else {
+      const test = new ExerciseInfo({
+        "username": userData.username,
+        "description": req.body.description,
+        "duration": req.body.duration,
+        "date": checkedDate.toDateString()
+      });
+
+      test.save((error, data) => {
+        if (error) {
+          console.log("error saving=>", error);
+        } else {
+          console.log('saved exercise successfully');
+          res.json({
+            "_id": idToCheck,
+            "username": data.username,
+            "description": data.description,
+            "duration": data.duration,
+            "date": data.date
+          });
+        }
+      })
+    }
+
+  });
+});
+
+
+ // #3
+app.get('/api/users/:_id/logs', (req, res) => {
+  const {from, to, limit} = req.query;
+  let idJson = {"id": req.params._id};
+  let idToCheck = idJson.id;
+
+  // Check ID
+
+  UserInfo.findById(idToCheck, (err, data) => {
+    var query = {
+      username: data.username,
+    };
+
+    if (from !== undefined && to === undefined) {
+      query.date = {$gte: new Date(from)};
+    } else if (to !== undefined && from === undefined) {
+      query.date = {$lte: new Date(to)};
+    } else if (from !== undefined && to !== undefined) {
+      query.date = {$gte: new Date(from), $lte: new Date(to)};
+    }
+
+    let limitChecker = (limit) => {
+      let maxLimit = 100;
+      if (limit) {
+        return limit;
+      } else {
+        return maxLimit;
+      }
+    };
+
+    if (err) {
+      console.log("error with ID=> ", err);
+    } else {
+      ExerciseInfo.find((query), null, {limit: limitChecker(+limit)}, (error, exerciseData) => {
+        let loggedArray = [];
+        if (error) {
+          console.log("error with query=> ", error);
+        } else {
+          let documents = exerciseData;
+          loggedArray = documents.map(item => {
+            return {
+              description: item.description,
+              duration: item.duration,
+              date: item.date.toDateString()
+            }
+          });
+
+          const test = new LogInfo({
+            username: data.username,
+            count: loggedArray.length,
+            log: loggedArray
+          });
+
+          test.save((saveErr, saveData) => {
+            if (saveErr) console.log("error saving exercise=> ", saveErr);
+            else {
+              console.log("saved exercise successfully");
+              res.json({
+                "_id": idToCheck,
+                "username": data.username,
+                "count": data.count,
+                "log": loggedArray
+              })
+            }
+          });
+        }
+      });
+    }
+  })
+});
+
+  // #4
+app.get('/api/users', (req, res) => {
+  UserInfo.find({}, (err, data) => {
+    if (err) res.send("No Users");
+    else res.json(data);
+  });
 });
 
 // Config
